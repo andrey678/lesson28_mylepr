d068f864b1137084dab9f054cb091288b2b4cf19
andrey678 <drew6496@yandex.ru>
2020-05-31 19:29:25 +0300

    Add parameter validation

--- app.rb
+++ app.rb
@@ -26,6 +26,12 @@ get '/new' do
 end
 
 post '/new' do
+  
   content = params[:content]
+      if content.length <= 0
+        @error = 'Type post text'
+        return erb :new
+      end
+  
   erb "You typed #{content}"
 end   
